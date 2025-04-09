c3c78c5054d68663679d55c5a63263320ad7866c
Thibaut <thibaut.devigne@share-heritage.com>
2025-04-09 11:13:52 +0200

    feat: add and connect typeorm and postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5434,
+      username: 'postgres',
+      password: 'password',
+      database: 'strav-b',
+      autoLoadEntities: true,
+      synchronize: true, // À désactiver en production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
